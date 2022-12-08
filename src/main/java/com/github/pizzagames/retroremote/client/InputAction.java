0dfcedea9468ddcec9b8fc9d3c63040cc5896356
Marcelo Frau <marcelofrau@gmail.com>
2022-12-08 20:45:44 +0100

    Adding project and IDE settings

--- src/main/java/com/github/pizzagames/retroremote/client/InputAction.java
+++ src/main/java/com/github/pizzagames/retroremote/client/InputAction.java
@@ -1,14 +1,13 @@
 package com.github.pizzagames.retroremote.client;
 
 public class InputAction {
+
     public static final String KEY_DOWN = "KD";
     public static final String KEY_UP = "KU";
     public static final String MOUSE_DOWN = "MD";
     public static final String MOUSE_UP = "MU";
     public static final String MOUSE_MOVE = "MM";
     public static final String MOUSE_WHEEL = "MW";
-
-
 
     private final String action;
     private final int width;
@@ -19,16 +18,16 @@ public class InputAction {
     private final int keyCode;
     private final String keyValue;
 
-	public InputAction(String action, int width, int height, int x, int y, int delta, int keyCode, String keyValue) {
-		this.action = action;
-		this.width = width;
-		this.height = height;
-		this.x = x;
-		this.y = y;
-		this.delta = delta;
-		this.keyCode = keyCode;
-		this.keyValue = keyValue;
-	}
+    public InputAction(String action, int width, int height, int x, int y, int delta, int keyCode, String keyValue) {
+        this.action = action;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.delta = delta;
+        this.keyCode = keyCode;
+        this.keyValue = keyValue;
+    }
 
     public String getAction() {
         return action;
@@ -61,6 +60,5 @@ public class InputAction {
     public String getKeyValue() {
         return keyValue;
     }
-
 
 }
